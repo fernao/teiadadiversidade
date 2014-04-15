e3f902c3498bc027a68c28a45de6b3a36e528e07
Fernão Lopes <fernao@riseup.net>
2014-04-15 11:24:33 -0300

    alteracoes de imagens / preload + css de memoria

--- js/functions.js
+++ js/functions.js
@@ -18,4 +18,34 @@ jQuery(document).ready(function() {
 	clearTimeout(duration_cycle);
     });
     /*** fim headlines ***/
+
+    /* preload dos backgrounds rollover */
+    preload = function(img) {
+      imagem.src = img;
+    }
+
+    preload([
+    	'img/culturaviva.txt.png',
+	'img/programacao.txt.png',
+	'img/galeria.txt.png',
+	'img/tvteia.txt.png',
+	'img/radioteia.txt.png',
+	'img/colaborativa.txt.png',
+	'img/inscricoes.txt.png',
+	'img/localizacao.txt.png',
+	'img/localizacao.txt.png',
+	'img/imprensa.txt.png',
+	'img/contato.txt.png',
+	'img/culturaviva.txt.pq.png',
+	'img/programacao.txt.pq.png',
+	'img/galeria.txt.pq.png',
+	'img/tvteia.txt.pq.png',
+	'img/radioteia.txt.pq.png',
+	'img/colaborativa.txt.pq.png',
+	'img/inscricoes.txt.pq.png',
+	'img/localizacao.txt.pq.png',
+ 	'img/imprensa.txt.pq.png',
+	'img/contato.txt.pq.png'
+	]);	
 });
+
